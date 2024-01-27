e846f4f8ae8769b28f8c304208fe018e7b08afbc
IlMusu <mattia.musumeci@yahoo.it>
2024-01-27 14:57:30 +0100

    - Update to 1.20.4

--- src/main/java/com/jamieswhiteshirt/reachentityattributes/mixin/client/ClientPlayerInteractionManagerMixin.java
+++ src/main/java/com/jamieswhiteshirt/reachentityattributes/mixin/client/ClientPlayerInteractionManagerMixin.java
@@ -3,23 +3,26 @@ package com.jamieswhiteshirt.reachentityattributes.mixin.client;
 import com.jamieswhiteshirt.reachentityattributes.ReachEntityAttributes;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerInteractionManager;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.GameMode;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ClientPlayerInteractionManager.class)
 abstract class ClientPlayerInteractionManagerMixin {
     @Shadow @Final private MinecraftClient client;
+    @Shadow private GameMode gameMode;
 
-    @ModifyConstant(
-        method = "getReachDistance()F",
-        require = 2, allow = 2, constant = { @Constant(floatValue = 5.0F), @Constant(floatValue = 4.5F) })
-    private float getActualReachDistance(final float reachDistance) {
-        if (this.client.player != null) {
-            return (float) ReachEntityAttributes.getReachDistance(this.client.player, reachDistance);
-        }
-        return reachDistance;
+    @Redirect(
+            method = "getReachDistance()F",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;getReachDistance(Z)F"))
+    private float getActualReachDistance(boolean creative) {
+        float defaultReachDistance = PlayerEntity.getReachDistance(this.gameMode.isCreative());
+        if (this.client.player != null)
+            return (float) ReachEntityAttributes.getReachDistance(this.client.player, defaultReachDistance);
+        return defaultReachDistance;
     }
 }
